c1520ac5f47f78e4ed5a71938b7331f18e3a758a
LucasMRevel <72999739+LucasMRevel@users.noreply.github.com>
2022-02-24 15:05:54 -0300

    refactor: cambiar nomenclatura

--- src/store/modules/lista.js
+++ src/store/modules/lista.js
@@ -1,81 +1,96 @@
-export default {
-	namespaced: true,
-	state: {
+
+const state = {
 		//contador: 0,
 		series: [],
 			nuevaSerie: '',
 		
-	},
+	}
 
-	mutations: {
+const mutations = {
 
-		AGREGAR_SERIE(state, payload){
-			console.log(payload, "payload")
-			const serie = {
-				nombre: payload.nombre,
-				estado: false				
-			}
+	AGREGAR_SERIE(state, payload){
+		//console.log(payload, "payload")
+		const serie = {
+			nombre: payload.nombre,
+			estado: false				
+		}
 			state.series.push(serie)
 			state.nuevaSerie = "";
 			console.log(state.series)
 			console.log(state.nuevaSerie)
+			//localStorage.setItem('lasSeries', JSON.stringify('localStorage/getStateDef'))
 		},
 
-		COLORES_ETIQUETAS(state, index){
-			state.series[index].estado = true
-		},
-
-		BORRAR_TODO(state, index){
-			state.series.splice(index, 1)
-		},
-
-		TODO_VISTO(state){
-			state.series.forEach((serie) => serie.estado = true)
-		},
+	COLORES_ETIQUETAS(state, index){
+		state.series[index].estado = true
+			//localStorage.setItem('lasSeries', JSON.stringify('localStorage/getStateDef'))
 	},
 
-	actions:{
-
-		async borrar_todo({commit}, index){
-			await new Promise( (cancelar)=>{   
-				setTimeout( ()=>{
-					cancelar()
-				},100)
-			}) 
-			commit('BORRAR_TODO', index)
-		},
-
-		async nueva_serie({commit}, payload){
-			await new Promise( (aceptar)=>{   
-				setTimeout( ()=>{
-					aceptar()
-				},50)
-			})
-			commit('AGREGAR_SERIE', payload)       
-		},
-
-		async colores({commit}, index){
-			commit('COLORES_ETIQUETAS', index)
-		},
-		
-
-		async colorDefinitivo({commit}, index){
-			await new Promise( (aceptar)=>{   
-				setTimeout( ()=>{
-					aceptar()
-				},50)
-			})
-			commit('TODO_VISTO', index)       
-		},
-
+	BORRAR_TODO(state, index){
+		state.series.splice(index, 1)
+			//localStorage.setItem('lasSeries', JSON.stringify('localStorage/getStateDef'))
 	},
-	getters: {
 
-		estado: state => state.series.filter(series => series.estado),
-		
-		estadoDefinitivo: (state, getters) => getters.estado.length,
-	
-
-    },
-
+	TODO_VISTO(state){
+		state.series.forEach((serie) => serie.estado = true)
+			//localStorage.setItem('lasSeries', JSON.stringify('localStorage/getStateDef'))
+	},
 }
+
+const actions={
+
+	async borrar_todo({commit}, index){
+		await new Promise( (cancelar)=>{   
+			setTimeout( ()=>{
+				cancelar()
+			},100)
+		}) 
+		commit('BORRAR_TODO', index)
+	},
+
+	async nueva_serie({commit}, payload){
+		await new Promise( (aceptar)=>{   
+			setTimeout( ()=>{
+				aceptar()
+			},50)
+		})
+		commit('AGREGAR_SERIE', payload)       
+	},
+
+	async colores({commit}, index){
+		commit('COLORES_ETIQUETAS', index)
+	},
+		
+
+	async colorDefinitivo({commit}, index){
+		await new Promise( (aceptar)=>{   
+			setTimeout( ()=>{
+				aceptar()
+			},50)
+		})
+		commit('TODO_VISTO', index)       
+	},
+}
+
+const	getters= {
+
+	estado: state => state.series.filter(series => series.estado),
+		
+	estadoDefinitivo: (state, getters) => getters.estado.length,
+
+	title: state => state.title,
+
+		/*series: (state, rootGetters, root) =>{
+			console.log(root['localStorage'], rootGetters)
+			return root['localStorage/getItem', "lasSeries"]
+			
+		}*/
+}
+
+	export default {
+		namespaced: true,
+		state,
+		getters,
+		actions,
+		mutations
+	}
